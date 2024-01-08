de1b6944d39876c284cb02d0975029da5ebe01f3
seed-yoo <lukdown10@naver.com>
2024-01-08 15:44:05 +0900

    .

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -8,10 +8,12 @@ public class Ex08 {
 		 배열을 사용하여 1~45까지의 숫자 중 임의의 6개의 숫자를 출력하세요-미니로또(중복체크 할 것)
 		 */
 		int[] ln = new int[6];
-        boolean[] ns = new boolean[46]; 
+        boolean[] ns = new boolean[46];		// 중복 체크용
 
         for (int i = 0; i < ln.length; i++) {
+        	
             int rn;
+            
             do {
                 rn = (int) (Math.random() * 45) + 1;
             } while (ns[rn]); 
